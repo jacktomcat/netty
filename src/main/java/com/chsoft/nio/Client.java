c80d76388255558517453c1627f2541c224113c4
zhuhuihui <jackjboss@gmail.com>
2021-06-20 22:38:11 +0800

    nio

---
+++ src/main/java/com/chsoft/nio/Client.java
@@ -0,0 +1,36 @@
+package com.chsoft.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+/**
+ * @description:
+ * @author: Mr.Zhu
+ * @create: 2021-06-20 18:57
+ **/
+public class Client {
+
+    public static void main(String[] args) throws IOException {
+        try {
+            SocketChannel socketChannel = SocketChannel.open();
+            socketChannel.connect(new InetSocketAddress("127.0.0.1", 8000));
+
+            ByteBuffer writeBuffer = ByteBuffer.allocate(32);
+            ByteBuffer readBuffer = ByteBuffer.allocate(32);
+
+            writeBuffer.put("hello".getBytes());
+            writeBuffer.flip();
+
+            while (true) {
+                writeBuffer.rewind();
+                socketChannel.write(writeBuffer);
+                readBuffer.clear();
+                socketChannel.read(readBuffer);
+            }
+        } catch (IOException e) {
+        }
+    }
+
+}
